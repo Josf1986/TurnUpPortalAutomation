609e8a5d916b133c77928c236c9e473bb3f6c954
Joseph Antony <joseph.josf@gmail.com>
2023-10-11 12:46:20 +1300

    Updated with Nunit implementation and Assertion

--- TurnUpPortalAutomation/Pages/TMPage.cs
+++ TurnUpPortalAutomation/Pages/TMPage.cs
@@ -1,9 +1,5 @@
-﻿using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace TurnUpPortalAutomation.Pages
 {
@@ -38,7 +34,7 @@ namespace TurnUpPortalAutomation.Pages
             IWebElement SaveButton = driver.FindElement(By.Id("SaveButton"));
             SaveButton.Click();
 
-            Thread.Sleep(5000);
+            Thread.Sleep(2000);
 
             // Check if a new Time record has been created successfully
             IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
@@ -46,25 +42,21 @@ namespace TurnUpPortalAutomation.Pages
 
             IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
 
-            if (newCode.Text == "Test Practise")
-            {
-                Console.WriteLine("New time record has been created successfully");
-            }
-            else
-            {
-                Console.WriteLine("Time record has not been created");
-            }
+            Assert.That(newCode.Text == "Test Practise", "Time record has not been created");
+            
         }
         public void EditTimeRecord(IWebDriver driver) 
         {
             //Edit new Time Record
             Thread.Sleep(3000);
-
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+           
             IWebElement EditButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
            
             EditButton.Click();
 
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
             driver.FindElement(By.Id("Code")).Clear();
             driver.FindElement(By.Id("Code")).SendKeys("Test Edit Practise");
 
@@ -76,38 +68,30 @@ namespace TurnUpPortalAutomation.Pages
 
             driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span")).Click();
 
-            Thread.Sleep(3000);
+            Thread.Sleep(5000);
 
             IWebElement newEditCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
 
-            if (newEditCode.Text == "Test Edit Practise")
-            {
-                Console.WriteLine("New time record has been edited successfully");
-            }
-            else
-            {
-                Console.WriteLine("Time record has not been edited");
-            }
+            Assert.That(newEditCode.Text == "Test Edit Practise", "Time record has not been edited");
+
         }
         public void DeleteTimeRecord(IWebDriver driver) 
         {
             //Delete the new Time Record
-
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+         
             IWebElement DeleteButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+          
             DeleteButton.Click();
+            Thread.Sleep(1000);
             driver.SwitchTo().Alert().Accept();
             Thread.Sleep(2000);
 
             IWebElement DeletedCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+            Assert.That(DeletedCode.Text != "Test Edit Practice", "Edited time record has not been deleted");
 
-            if (DeletedCode.Text == "Test Edit Practise")
-            {
-                Console.WriteLine("Edited time record has not been deleted");
-            }
-            else
-            {
-                Console.WriteLine("Edited time record has been deleted successsfully");
-            }
+           
         }
     }
 }
---
+++ TurnUpPortalAutomation/Tests/TMTests.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnUpPortalAutomation.Pages;
+using NUnit.Framework;
+using TurnUpPortalAutomation.Utilities;
+
+namespace TurnUpPortalAutomation.Tests
+{
+    [TestFixture]
+    public class TMTests : CommonDriver
+    {
+        [SetUp]
+        public void SetUpTM()
+        {   
+            // Open chrome browser
+            driver = new ChromeDriver();
+
+            //Login page object initilization and definition
+            LoginPage loginPageObj = new LoginPage();
+            loginPageObj.LoginActions(driver);
+
+        }
+        [Test, Order(1), Description("This test creates a new Time record with valid data")]  
+        public void TestCreateTimeRecord()
+        {
+            //Home page object initilization and definition
+            HomePage homePageObj = new HomePage();
+            homePageObj.GoToTMPage(driver);
+
+            //TM page object initilization and definition
+            TMPage tmPageObj = new TMPage();
+            tmPageObj.CreateTimeRecord(driver);
+        }
+
+        [Test, Order(2), Description("This test updates an existing Time record with valid data")]      
+        public void TestEditTimeRecord()
+        {
+            TMPage tmPageObj = new TMPage();
+            tmPageObj.EditTimeRecord(driver);
+        }
+
+        [Test, Order(3), Description("This test deletes an existing Time record")]
+        public void TestDeleteTimeRecord()
+        {
+            TMPage tmPageObj = new TMPage();
+            tmPageObj.DeleteTimeRecord(driver);
+        }
+        [TearDown]
+        public void CloseTestRun()
+        { 
+            driver.Quit();
+        }
+     
+        
+        
+        
+        
+        
+        
+      
+
+     
+    }
+}
